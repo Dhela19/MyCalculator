21afcdbd474c6d16e34c9241f87fd68ef984ebc5
Ubongabasi Jerome <87214555+Dhela19@users.noreply.github.com>
2023-01-24 10:30:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,51 +1,83 @@
+//Play Audio on click
+
+const audio = new Audio("./click.mp3");
+function click() {
+    audio.play();
+    document.getElementById("cont").style="background:white;"
+  };
+  function aftermath(){
+  document.getElementById("cont").style="background:#aaffdfa1"
+  }
 var display = (document.getElementById("disp"));
 var results = (document.getElementById("res"));
 function add()
 {
+    click()
     display.value +=("+")
+    aftermath()
 }
 function subt(){
+    click()
     display.value +=("-")
+    aftermath()
 }
 function mult(){
+    click()
     display.value +=("*")
+    aftermath()
 }
 function nine(){
+    click()
     display.value +=("9")
+    
 }
 function eight(){
+    click()
     display.value +=("8")
+    
 }
 function seven(){
+    click()
     display.value +=("7")
+    
 }
 function six(){
+    click()
     display.value +=("6")
+    
     }
 
 function div(){
+    click()
     display.value +=("/")
 }
 function five(){
+    click()
     display.value +=("5")
 }
 function four(){
+    click()
     display.value +=("4")
 }
 function three(){
+    click()
     display.value +=("3")
 }
 function two(){
+    click()
     display.value +=("2")
 }
 function one(){
+    click()
     display.value +=("1")
 }
 function zero(){
+    click()
     display.value +=("0")
 }
 //Equals operation
 function equals(){
+    click()
     function looseJsonParse(display) {
   return eval?.(`"use strict";(${display.value})`);
 }
@@ -53,22 +85,13 @@ results.value=(looseJsonParse(display));
     display.style=("display:none");
     results.style=("display:inline");
     //return results.value=results
-    
+    aftermath()
 }
 function mc(){
+    click()
 display.style=("display:inline")
     display.value=("")
     results.style=("display:none")
     results.value=("")
+    aftermath()
 }
-//Play Audio on click
-
-const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-const buttons = document.querySelectorAll("btns");
-
-buttons.forEach(button => {
-  buttons.addEventListener("click", () => {
-    audio.play();
-  });
-});
-//
